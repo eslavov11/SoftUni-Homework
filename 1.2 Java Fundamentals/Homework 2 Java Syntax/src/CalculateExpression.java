fdc0b5b2d810f4dbe89257bc03671e311520d558
Edward Slavov <e.slavov11@gmail.com>
2015-10-15 19:39:34 +0300

    java

--- 1.2 Java Fundamentals/Homework 2 Java Syntax/src/CalculateExpression.java
+++ 1.2 Java Fundamentals/Homework 2 Java Syntax/src/CalculateExpression.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class CalcilateExpression{
+public class CalculateExpression {
 	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 		float a = Float.parseFloat(input.nextLine());
 		float b = Float.parseFloat(input.nextLine());
 		float c = Float.parseFloat(input.nextLine());
-		System.out.print("F1 result: " + Math.pow(a*a + b*b)/(a*a - b*b),(a+b+c)/Math.sqrt(c)); 
-		System.out.print("; F2 result: " +  Math.pow((a*a + b*b - c*c*c),(a-b)));
-		System.out.println("; Diff:" + (((a+b+c)/3) - 
-				((Math.pow(a*a + b*b)/(a*a - b*b),(a+b+c)/Math.sqrt(c) + Math.pow((a*a + b*b - c*c*c),(a-b))) /2));
+		double f1 = Math.pow((a*a + b*b)/(a*a - b*b),((a+b+c)/Math.sqrt(c)));
+		double f2 = Math.pow((a*a + b*b - c*c*c),(a-b));
+		System.out.printf("F1 result: %.2f", f1); 
+		System.out.printf("; F2 result: %.2f", f2);
+		System.out.printf("; Diff: %.2f",  Math.abs((((a+b+c)/3) - ((f1+f2)/2))));
 		
 //		((a2 + b2) / (a2 – b2))(a + b + c) / √c             (a2 + b2 - c3)(a – b)
 	}
---
+++ 1.2 Java Fundamentals/Homework 2 Java Syntax/src/MagicExchanegableWords.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class MagicExchanegableWords {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	}
+}

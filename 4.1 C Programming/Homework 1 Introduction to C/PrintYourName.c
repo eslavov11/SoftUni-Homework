793e9fc2e418373921f4a46c5f884b61c57bbd9c
Edward Slavov <e.slavov11@gmail.com>
2015-10-23 23:13:25 +0300

    Update PrintYourName.c

--- 4.1 C Programming/Homework 1 Introduction to C/PrintYourName.c
+++ 4.1 C Programming/Homework 1 Introduction to C/PrintYourName.c
@@ -1,6 +1,6 @@
-//#include <stdio.h>
-//int main()
-//{
-//    printf("My name is Edward.\n");
-//    return 0;
-//}
+#include <stdio.h>
+int main()
+{
+    printf("My name is Edward.\n");
+    return 0;
+}

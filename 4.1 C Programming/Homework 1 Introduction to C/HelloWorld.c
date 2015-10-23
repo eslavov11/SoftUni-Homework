859c37a024f23dc15de8459eccb3ec5d7c2fab6e
Edward Slavov <e.slavov11@gmail.com>
2015-10-23 23:31:34 +0300

    homework

--- 4.1 C Programming/Homework 1 Introduction to C/HelloWorld.c
+++ 4.1 C Programming/Homework 1 Introduction to C/HelloWorld.c
@@ -1,7 +1,7 @@
-//#include <stdio.h>
-// 
-//int main()
-//{
-//  printf("Hello C!\n");
-//  return 0;
-//}       
+#include <stdio.h>
+ 
+int main()
+{
+  printf("Hello C!\n");
+  return 0;
+}       

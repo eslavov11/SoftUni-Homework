46ae8f9b79c04e1914d27bfc411eb1503c72099b
Edward Slavov <e.slavov11@gmail.com>
2015-11-26 17:39:24 +0200

    encapsulation

---
+++ 1.3 Object-oriented programming/Exercise 2 Encapsulation and Polymorphism/AnimalFarm/AnimalFarm.cs
@@ -0,0 +1,13 @@
+﻿namespace AnimalFarm
+{
+    using System;
+
+    public class AnimalFarm
+    {
+        public static void Main()
+        {
+            Chicken chicken = new Chicken("Mara", 3);
+            Console.WriteLine(chicken.ProductPerDay);
+        }
+    }
+}
---
+++ 1.3 Object-oriented programming/Exercise 2 Encapsulation and Polymorphism/AnimalFarm/Chicken.cs
@@ -0,0 +1,20 @@
+﻿namespace AnimalFarm
+{
+    class Chicken : Animal
+    {
+        public Chicken(string name, int age)
+            : base(name, age)
+        {
+        }
+
+        public override Product ProduceProduct()
+        {
+            return new Product();
+        }
+
+        public override double GetHumanAge()
+        {
+            return this.Age * 11;
+        }
+    }
+}

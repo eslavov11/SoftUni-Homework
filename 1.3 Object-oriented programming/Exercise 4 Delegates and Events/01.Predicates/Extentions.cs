c5953b181bfbab221eff33dd68476cab32bc7660
Edward Slavov <e.slavov11@gmail.com>
2015-12-01 17:24:47 +0200

    delegates

---
+++ 1.3 Object-oriented programming/Exercise 4 Delegates and Events/01.Predicates/Extentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Predicates
+{
+    public static class Extentions
+    {
+        public static T FirstOrDef<T>(this IEnumerable<T> collection, Func<T, bool> select)
+        {
+            foreach (var element in collection)
+            {
+                if (select(element))
+                {
+                    return element;
+                }
+            }
+            return default(T);
+        }
+    }
+}
---
+++ 1.3 Object-oriented programming/Exercise 4 Delegates and Events/01.Predicates/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Predicates
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("asfasf");
+            list.Add("Bogo");
+            list.Add("asdfa");
+            Console.WriteLine(list.FirstOrDef(x => x == "Bogdo"));
+        }
+    }
+}

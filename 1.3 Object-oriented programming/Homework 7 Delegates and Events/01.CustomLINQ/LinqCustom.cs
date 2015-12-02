af4cbdf4e02771e7db532a767e0c3dd4d8d8beb2
Edward Slavov <e.slavov11@gmail.com>
2015-12-02 17:41:28 +0200

    delegates and events

---
+++ 1.3 Object-oriented programming/Homework 7 Delegates and Events/01.CustomLINQ/LinqCustom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CustomLINQ
+{
+    public static class LinqCustom
+    {
+        public static IEnumerable<T> WhereNot<T>(this IEnumerable<T> collection, Func<T, bool> condition)
+        {
+            var resultCollection = new List<T>();
+            foreach (var element in collection)
+            {
+                if (!condition(element))
+                {
+                    resultCollection.Add(element);
+                }
+            }
+            return resultCollection;
+        }
+
+        public static TSelector Max<TSource, TSelector>(
+            this IEnumerable<TSource> collection,
+            Func<TSource, TSelector> selector)
+            where TSelector : IComparable<TSelector>
+        {
+            TSelector max = selector(collection.First());
+            foreach (var element in collection)
+            {
+                
+                if (selector(element).CompareTo(max) == 1)
+                {
+                    max = selector(element);
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ 1.3 Object-oriented programming/Homework 7 Delegates and Events/01.CustomLINQ/Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CustomLINQ
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // WhereNot example
+            var nums = new List<int>()
+            {
+                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var filteredCollection = nums.WhereNot(x => x % 2 == 0);
+
+            Console.WriteLine(string.Join(", ", filteredCollection));
+
+            var students = new List<Student>()
+            {
+                new Student("Asya", 6),
+                new Student("Ivan", 4),
+                new Student("Penka", 5),
+                new Student("Grigor", 3)
+            };
+            
+            Console.WriteLine(students.Max(student => student.Grade));
+            Console.WriteLine(students.Max(student => student.Name));
+            
+        }
+    }
+
+
+    public class Student
+    {
+        public Student(string name, int grade)
+        {
+            this.Name = name;
+            this.Grade = grade;
+        }
+
+        public string Name { get; set; }
+
+        public int Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Grade: {this.Grade}";
+        }
+    }
+}

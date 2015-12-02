d05c61e5d751365bb56377a29a2fa66c6f9fd03a
Edward Slavov <e.slavov11@gmail.com>
2015-12-02 17:12:57 +0200

    exercise delegates

---
+++ 1.3 Object-oriented programming/Exercise 4 Delegates and Events/02.Func/Extentions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Func
+{
+    public static class Extentions
+    {
+        public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> collection, Func<T, bool> conditionFunc)
+        {
+            var resultCollection = new List<T>();
+            foreach (T element in collection)
+            {
+                if (conditionFunc(element))
+                {
+                    resultCollection.Add(element);
+                }
+            }
+            return resultCollection;
+        }
+    }
+}
---
+++ 1.3 Object-oriented programming/Exercise 4 Delegates and Events/02.Func/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new int[]
+            {
+                5,6,7,8,15, 45
+            };
+
+            var biggerThanSeven = list.TakeWhile(x => x > 7);
+            foreach (var VARIABLE in biggerThanSeven)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+        }
+    }
+}

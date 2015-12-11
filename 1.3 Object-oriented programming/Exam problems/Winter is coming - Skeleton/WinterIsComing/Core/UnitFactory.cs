cce57d5ec706e8878d974fea0dc7c69bc0c8b758
Edward Slavov <e.slavov11@gmail.com>
2015-12-11 16:42:45 +0200

    exam

---
+++ 1.3 Object-oriented programming/Exam problems/Winter is coming - Skeleton/WinterIsComing/Core/UnitFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace WinterIsComing.Core
+{
+    using System;
+    using Contracts;
+    using Models;
+
+    public static class UnitFactory
+    {
+        public static IUnit Create(UnitType type, string name, int x, int y)
+        {
+            switch (type)
+            {
+                // TODO: Implement units
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}

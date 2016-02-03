b80ff2cee92b7722a1cdd5e59971dc6536dd0376
Edward Slavov <e.slavov11@gmail.com>
2016-02-03 14:27:31 +0200

    unit tests

---
+++ 1.4 High-Quality Code/Exams/ChepelareHotelBookingSystem/HotelBookingSystem.Tests/RepositoryTests.cs
@@ -0,0 +1,124 @@
+﻿namespace HotelBookingSystem.Tests
+{
+    using System.Security.Policy;
+
+    using ChepelareHotelBookingSystem.Data;
+    using ChepelareHotelBookingSystem.Enums;
+    using ChepelareHotelBookingSystem.Models;
+    using ChepelareHotelBookingSystem.Utilities;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void TestGet_InitializeValidUsers_UserShouldNotBeNull()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password123", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(2);
+
+            // Assert
+            Assert.IsNotNull(user, "User should not be null.");
+        }
+
+        [TestMethod]
+        public void TestGet_InitializeValidUsers_UsernamesShouldMatch()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password123", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(2);
+
+            // Assert
+            Assert.AreEqual("username2", user.Username, "Usernames don't match.");
+        }
+
+        [TestMethod]
+        public void TestGet_InitializeValidUsers_PasswordsShouldMatch()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password1234", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(2);
+
+            // Assert
+            Assert.AreEqual(HashUtilities.GetSha256Hash("password1234"), user.PasswordHash, "Passwords don't match.");
+        }
+
+        [TestMethod]
+        public void TestGet_InitializeValidUsers_RolesShouldMatch()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password123", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(2);
+
+            // Assert
+            Assert.AreEqual(Roles.VenueAdmin, user.Role, "Users roles don't match.");
+        }
+
+        [TestMethod]
+        public void TestGet_InitializeNoUsers_ShouldReturnNull()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+
+            // Act
+            var user = roomsRepo.Get(1);
+
+            // Assert
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void TestGet_NegativeId_ShouldReturnNull()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password123", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(-1);
+
+            // Assert
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void TestGet_InvalidPositiveId_ShouldReturnNull()
+        {
+            // Arrange
+            var roomsRepo = new UserRepository();
+            roomsRepo.Add(new User("username1", "password123", Roles.User));
+            roomsRepo.Add(new User("username2", "password123", Roles.VenueAdmin));
+            roomsRepo.Add(new User("username3", "password123", Roles.User));
+
+            // Act
+            var user = roomsRepo.Get(4);
+
+            // Assert
+            Assert.IsNull(user);
+        }
+    }
+}
---
+++ 1.4 High-Quality Code/Exams/ChepelareHotelBookingSystem/HotelBookingSystem.Tests/VenuesControllerTests.cs
@@ -0,0 +1,68 @@
+﻿namespace HotelBookingSystem.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Security.Policy;
+
+    using ChepelareHotelBookingSystem.Controllers;
+    using ChepelareHotelBookingSystem.Data;
+    using ChepelareHotelBookingSystem.Enums;
+    using ChepelareHotelBookingSystem.Models;
+    using ChepelareHotelBookingSystem.Utilities;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VenuesControllerTests
+    {
+        [TestMethod]
+        public void TestAll_InitializeValidVenues_ViewShouldNotBeNull()
+        {
+            // Arrange
+            var owner = new User("shefa", "mnMiELud123", Roles.VenueAdmin);
+            var data = new HotelBookingSystemData();
+            data.RepositoryWithVenues.Add(new Venue("Hotel1", "SOfia", "Nope", owner));
+            data.RepositoryWithVenues.Add(new Venue("Hotel2", "Plovdiv", "AbeImaNqkvo", null));
+            data.RepositoryWithVenues.Add(new Venue("Hotel3", "Vraca", "NeMoaGonamerq", owner));
+
+            // Act
+            var view = new VenuesController(data, owner).All().Display();
+
+            // Assert
+            Assert.IsNotNull(view, "Venues view should not be null.");
+        }
+
+        [TestMethod]
+        public void TestAll_InitializeNoVenues_ViewShouldHaveNoVenues()
+        {
+            // Arrange
+            var owner = new User("shefa", "mnMiELud123", Roles.VenueAdmin);
+            var data = new HotelBookingSystemData();
+            var controller = new VenuesController(data, owner);
+
+            // Act
+            var view = controller.All().Display();
+
+            // Assert
+            Assert.AreEqual("There are currently no venues to show.", view);
+        }
+
+        [TestMethod]
+        public void TestAll_InitializeOneVenue_ViewShouldBeEqual()
+        {
+            // Arrange
+            string expected = "*[1] Hotel1, located at SOfia" + 
+                Environment.NewLine + 
+                "Free rooms: 0";
+            var owner = new User("shefa", "mnMiELud123", Roles.VenueAdmin);
+            var data = new HotelBookingSystemData();
+            data.RepositoryWithVenues.Add(new Venue("Hotel1", "SOfia", "Nope", owner));
+
+            // Act
+            var view = new VenuesController(data, owner).All().Display();
+
+            // Assert
+            Assert.AreEqual(expected, view);
+        }
+    }
+}

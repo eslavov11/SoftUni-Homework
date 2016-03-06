5823c4c39e649ed1c40f328e4bd3e5b021e750fa
Edward Slavov <e.slavov11@gmail.com>
2016-03-06 23:16:11 +0200

    basics exam

---
+++ 0.1 Programming basics/Exams/Feb21-Exam-Programming-basics/Feb21-Exam-Programming-basics/HungryGarfield.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+public class HungryGarfield
+{
+    public static void Main()
+    {
+        decimal money = decimal.Parse(ReadLine()),
+         rate = decimal.Parse(ReadLine()),
+         pizzaPrice = decimal.Parse(ReadLine()),
+         lasagnaPrice = decimal.Parse(ReadLine()),
+         sandwichPrice = decimal.Parse(ReadLine()),
+         pizzaQuantity = decimal.Parse(ReadLine()),
+         lasagnaQuantity = decimal.Parse(ReadLine()),
+         sandwichQuantity = decimal.Parse(ReadLine()),
+         moneySpent = (pizzaPrice / rate * pizzaQuantity) +
+            (lasagnaPrice / rate * lasagnaQuantity) +
+            (sandwichPrice / rate * sandwichQuantity);
+
+        WriteLine(
+            money >= moneySpent
+                ? $"Garfield is well fed, John is awesome. Money left: ${(money - moneySpent):F2}."
+                : $"Garfield is hungry. John is a badass. Money needed: ${(moneySpent - money):F2}.");
+    }
+}

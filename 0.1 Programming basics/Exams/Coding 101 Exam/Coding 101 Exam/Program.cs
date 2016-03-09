0784ec03f65204eb61215c7c7b664169fccfa0a9
Edward Slavov <e.slavov11@gmail.com>
2016-03-09 22:03:41 +0200

    homework

---
+++ 0.1 Programming basics/Exams/Coding 101 Exam/Coding 101 Exam/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double height = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine()) - 1;
+        int tablesPerRow = Convert.ToInt32(Math.Floor(width / 0.7));
+        int rows = Convert.ToInt32(Math.Floor(height / 1.2));
+        Console.WriteLine(tablesPerRow * rows - 3);
+    }
+}
+

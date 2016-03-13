f0e68720aebf528e027f7065f8cb49d7828ba23c
Edward Slavov <e.slavov11@gmail.com>
2016-03-13 23:18:10 +0200

    templating and routing

--- 2.4 JavaScript Applications/Homework 4 Consuming REST/Homework 4 Consuming REST/src/app.js
+++
@@ -1,8 +0,0 @@
-var app = app || {};
-
-app.requester.config('kid_bkeREc9VJZ', 'e0a07a1765cb4b4cbfaf3dac70156325');
-var userRequester = new app.UserRequester();
-//userRequester.signUp('peter', 'pass123');
-userRequester.login('peter', 'pass123');
-
-app.getBooks();
